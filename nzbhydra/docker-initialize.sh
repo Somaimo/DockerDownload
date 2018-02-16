89f9b52f3dadd1e927befdf5e3a5002270eebdfc
Nomaimo <camao@bluewin.ch>
2018-02-16 22:48:08 +0100

    error correction

--- nzbhydra/docker-initialize.sh
+++ nzbhydra/docker-initialize.sh
@@ -12,5 +12,5 @@ echo "starting nzb hydra with correct env options"
 docker create --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
 -v $nfs_mountPoint/$nfs_dropfolder:/config \
--e TZ=Europe/Zurich
+-e TZ=Europe/Zurich \
 -p 5075:5075
