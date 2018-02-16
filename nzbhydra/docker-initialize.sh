5429fe33147e41c690e2d57e259fd09f826f38c4
Nomaimo <camao@bluewin.ch>
2018-02-16 22:53:58 +0100

    added docker

--- nzbhydra/docker-initialize.sh
+++ nzbhydra/docker-initialize.sh
@@ -9,9 +9,4 @@ nfs_app=nzbhydra
 nfs_dropFolder=dropfolder
 
 echo "starting nzb hydra with correct env options"
-docker create --name=$app_name \
--v $nfs_mountPoint/$nfs_app:/config \
--v $nfs_mountPoint/$nfs_dropfolder:/config \
--e PGID=$gid -e PUID=$uid \
--e TZ=Europe/Zurich \
--p 5075:5075
+docker create --name=$app_name -v $nfs_mountPoint/$nfs_app:/config -v $nfs_mountPoint/$nfs_dropfolder:/config -e PGID=$gid -e PUID=$uid -e TZ=Europe/Zurich -p 5075:5075
