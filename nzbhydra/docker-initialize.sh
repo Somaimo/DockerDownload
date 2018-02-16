407810250ab4d9b47ba67300a542588119a5cd4b
Nomaimo <camao@bluewin.ch>
2018-02-16 22:50:16 +0100

    added user and group id mapping to initialize file.

--- nzbhydra/docker-initialize.sh
+++ nzbhydra/docker-initialize.sh
@@ -12,5 +12,6 @@ echo "starting nzb hydra with correct env options"
 docker create --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
 -v $nfs_mountPoint/$nfs_dropfolder:/config \
+-e PGID=$gid -e PUID=$uid \
 -e TZ=Europe/Zurich \
 -p 5075:5075
