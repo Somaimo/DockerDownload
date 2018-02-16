3368e4dbd6d351a0f7ee45a4720b48a889a000a9
Nomaimo <camao@bluewin.ch>
2018-02-16 22:46:11 +0100

    added docker initialization script for nzbhydra

---
+++ nzbhydra/docker-initialize.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+$uid=1002
+$gid=1006
+
+$app_name=hydra
+$nfs_mountPoint=/srv/tiger/svcs
+$nfs_app=nzbhydra
+$nfs_dropFolder=dropfolder
+
+echo "starting nzb hydra with correct env options"
+docker create --name=$app_name \
+-v $nfs_mountPoint/$nfs_app:/config \
+-v $nfs_mountPoint/$nfs_dropfolder:/config \
+-e TZ=Europe/Zurich
+-p 5075:5075
