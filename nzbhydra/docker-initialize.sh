aca0edc155efab30a0032789c3d53366f3c880af
Nomaimo <camao@bluewin.ch>
2018-02-16 23:36:09 +0100

    corrections

--- nzbhydra/docker-initialize.sh
+++ nzbhydra/docker-initialize.sh
@@ -11,7 +11,7 @@ nfs_dropFolder=dropfolder
 echo "starting nzb hydra with correct env options"
 docker create --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
--v $nfs_mountPoint/$nfs_dropfolder:/config \
+-v $nfs_mountPoint/$nfs_dropfolder:/downloads \
 -e PGID=$gid -e PUID=$uid \
 -e TZ=Europe/Zurich \
 -p 5075:5075 \
