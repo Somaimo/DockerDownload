717d6e80ce2d17cad367945be3ef366daef10b64
Nomaimo <camao@bluewin.ch>
2018-02-18 13:13:32 +0100

    updated docker initalize script for hydra 2

--- nzbhydra/docker-initialize.sh
+++ nzbhydra/docker-initialize.sh
@@ -3,16 +3,16 @@
 uid=1002
 gid=1006
 
-app_name=hydra
+app_name=hydra2
 nfs_mountPoint=/srv/tiger/svcs
 nfs_app=nzbhydra
 nfs_dropFolder=dropfolder
 
 echo "starting nzb hydra with correct env options"
-docker create --name=$app_name \
+docker run -d --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
 -v $nfs_mountPoint/$nfs_dropfolder:/downloads \
 -e PGID=$gid -e PUID=$uid \
 -e TZ=Europe/Zurich \
 -p 5075:5075 \
-nzbhydra
+nzbhydra2
