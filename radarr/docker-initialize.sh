5b4bd126b795fbafee973f30a2fa079ef7cf001d
Somaimo <camao@bluewin.ch>
2018-02-25 18:29:35 +0100

    corrected bash scripts

--- radarr/docker-initialize.sh
+++ radarr/docker-initialize.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-uid=1002
-gid=1006
+# The user is the user marc on freenas and the media_rw group
+uid=1001
+gid=1008
 
 app_name=radarr
 nfs_mountPoint=/srv/tiger/svcs
 nfs_app=radarr
 nfs_downloadFolder=complete
-nfs_moviesFolder=/srv/tiger/movies
+nfs_moviesFolder=/srv/tiger/
 nfs_dropFolder=dropfolder
 
 echo "starting ${app_name} with correct env options"
@@ -15,6 +16,7 @@ docker run -d --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
 -v $nfs_mountPoint/$nfs_downloadFolder/movies:/downloads \
 -v $nfs_moviesFolder:/movies \
+-v /etc/localtime:/etc/localtime:ro \
 -e PGID=$gid -e PUID=$uid \
 -e TZ=Europe/Zurich \
 -p 7878:7878 \
--- sonarr/docker-initialize.sh
+++ sonarr/docker-initialize.sh
@@ -2,8 +2,8 @@
 
 # Sonarr Bash script
 
-uid=1002
-gid=1006
+uid=1001
+gid=1008
 
 app_name=sonarr
 nfs_mountPoint=/srv/tiger/svcs
@@ -16,7 +16,7 @@ echo "starting ${app_name} with correct env options"
 docker run -d --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
 -v $nfs_mountPoint/$nfs_downloadFolder/series:/downloads \
--v $nfs_seriesFolder:/series \
+-v $nfs_seriesFolder:/tv \
 -e PGID=$gid -e PUID=$uid \
 -e TZ=Europe/Zurich \
 -p 8989:8989 \
