379e01582096ef48822f6a3b5d4dabcacc8c87a0
Nomaimo <camao@bluewin.ch>
2018-03-02 19:29:11 +0100

    trying new thing with sabnzbd for media sharing

--- sabnzbdplus/docker-initialize.sh
+++ sabnzbdplus/docker-initialize.sh
@@ -6,15 +6,17 @@ gid=1006
 app_name=sabnzbd
 nfs_mountPoint=/srv/tiger/svcs
 nfs_app=sab-new
+nfs_mountMediaPoint=/srv/tiger
 nfs_downloadFolder=complete
 nfs_incompleteFolder=incomplete
 
 echo "starting ${app_name} with correct env options"
 docker run -d --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
--v $nfs_mountPoint/$nfs_downloadFolder:/downloads \
+-v $nfs_mountMediaPoint/movies:/downloads/movies \
+-v $nfs_mountMediaPoint/series:/downloads/series \
 -v $nfs_mountPoint/$nfs_incompleteFolder:/incomplete-downloads \
 -e PGID=$gid -e PUID=$uid \
 -e TZ=Europe/Zurich \
 -p 8080:8080 \
-linuxserver/sabnzbd
+sabnzbdplus
