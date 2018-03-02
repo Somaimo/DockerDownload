84cb2bf11eef6f23f08656a22c2e589c932c1edf
Nomaimo <camao@bluewin.ch>
2018-03-02 19:35:23 +0100

    expanded mountpoints for sabnzbd to include apps and other

--- sabnzbdplus/docker-initialize.sh
+++ sabnzbdplus/docker-initialize.sh
@@ -15,6 +15,8 @@ docker run -d --name=$app_name \
 -v $nfs_mountPoint/$nfs_app:/config \
 -v $nfs_mountMediaPoint/movies:/downloads/movies \
 -v $nfs_mountMediaPoint/series:/downloads/series \
+-v $nfs_mountPoint/$nfs_downloadFolder/apps:/downloads/apps \
+-v $nfs_mountPoint/$nfs_downloadFolder/other:/downloads/other \
 -v $nfs_mountPoint/$nfs_incompleteFolder:/incomplete-downloads \
 -e PGID=$gid -e PUID=$uid \
 -e TZ=Europe/Zurich \
